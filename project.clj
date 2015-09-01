2302804b64c92a571526a0a84d24b2597ba9c5a4
David Larsson <david.larsson@fripost.org>
2015-09-01 16:59:20 +0200

    Update project.clj
    Fixed project description and URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fundomain "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An application for computing fundamental domains for certain types of Fuchsian groups."
+  :url "http://github.com/DavidLarsson/fundomain"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
